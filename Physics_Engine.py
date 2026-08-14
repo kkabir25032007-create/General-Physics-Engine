4808310b4cbdd0612a8efce940ada0add57daa7b
Plaximus <kkabir25032007@gmail.com>
2026-08-14 23:00:13 +0530

    Refactor Physics classes and add electric forces

--- Physics_Engine.py
+++ Physics_Engine.py
@@ -1,4 +1,4 @@
-from vectogebra import vector
+from vectogebra.vector import vector
 
 
 class Physics:
@@ -16,12 +16,13 @@ class Physics:
         self.net_force = net_force if net_force is not None else vector(0,0,0)
     def add_force(self, force_vector):
         self.net_force = self.net_force + force_vector
-class NewtonianMechanics(Physics):
-    def __init__(self, mass, position, friction_co, restitution_co, gravity = None):
-        super().__init__(mass = mass, position = position)
+class Dynamics(Physics):
+    def __init__(self, mass, position, friction_co, restitution_co, gravity = None, charge = 0, electric_field = None):
+        super().__init__(mass = mass, position = position, charge = charge)
         self.friction_co = friction_co
         self.gravity = gravity if gravity is not None else vector(0,0,0)
         self.restitution_co = restitution_co
+        self.electric_field = electric_field if electric_field is not None else vector(0,0,0)
     def frictional_force(self):
         if abs(self.velocity) > 0:
             self.force_of_friction = (-1)*(self.velocity/abs(self.velocity))*self.mass*abs(self.gravity)
@@ -31,37 +32,44 @@ class NewtonianMechanics(Physics):
     def gravitational_force(self):
         self.force_of_gravity = self.mass * self.gravity
         self.add_force(self.force_of_gravity)
+    def electrostatic_force(self):
+        self.electric_force = self.electric_field * self.charge
+        self.add_force(self.electric_force)
     def collision(self, boundary_normal):
         initial_momentum = self.momentum
         final_momentum = initial_momentum - (1+self.restitution_co)*(self.momentum*boundary_normal)*(boundary_normal)
         self.momentum = final_momentum
     def particle_colllision(self, other, unit_body_normal):
-        self.unit_body_normal = (other.position - self.position)/abs(self.position - other.position)
+        displacement = other.position - self.position
+        distance_squared = displacement * displacement
+        radius_sum = self.radius + other.radius
+        if distance_squared > radius_sum * radius_sum:
+            return  # pair culling: spheres don't touch
+        if distance_squared == 0:
+            return  # coincident centres: no collision normal
+        self.distance_between_centre = abs(displacement)
+        self.unit_body_normal = displacement / self.distance_between_centre
         self.relative_velocity = (self.momentum/self.mass - other.momentum/other.mass)*self.unit_body_normal
-        self.distance_between_centre = abs(self.position - other.position)
-        penetration = self.radius + other.radius - self.distance_between_centre
-        if self.distance_between_centre > (self.radius + other.radius):
-            pass
-        elif self.distance_between_centre <= (self.radius + other.radius) and self.relative_velocity >= 0:
-            average_restitution_co = (self.restitution_co + other.restitution_co)/2
-            self.future_momentum = (self.mass)*((self.mass - average_restitution_co*other.mass)*(self.momentum/self.mass) + (((1 + average_restitution_co)*other.momentum)/(self.mass + other.mass)))
-            other.momentum = (other.mass)*((other.mass - average_restitution_co*self.mass)*(other.momentum/other.mass) + (((1 + average_restitution_co)*(self.momentum))/(self.mass + other.mass)))
-            self.momentum = self.future_momentum
-            penetration = 0.8 * penetration
-            self.penetration_share = (1/self.mass)/(1/self.mass + 1/other.mass)
-            other.penetration_share = (1/other.mass)/(1/self.mass + 1/other.mass)
-            self.position -= self.penetration_share*penetration*self.unit_body_normal
-            other.position += other.penetration_share*penetration*self.unit_body_normal
-        elif self.distance_between_centre <= (self.radius + other.radius) and self.relative_velocity < 0:
-            pass
+        if self.relative_velocity < 0:
+            return  # separating: no impulse
+        penetration = radius_sum - self.distance_between_centre
+        average_restitution_co = (self.restitution_co + other.restitution_co)/2
+        self.future_momentum = (self.mass)*(((self.mass - average_restitution_co*other.mass)*(self.momentum/self.mass) + (((1 + average_restitution_co)*other.momentum))/(self.mass + other.mass)))
+        other.momentum = (other.mass)*(((other.mass - average_restitution_co*self.mass)*(other.momentum/other.mass) + (((1 + average_restitution_co)*(self.momentum)))/(self.mass + other.mass)))
+        self.momentum = self.future_momentum
+        penetration = 0.8 * penetration
+        self.penetration_share = (1/self.mass)/(1/self.mass + 1/other.mass)
+        other.penetration_share = (1/other.mass)/(1/self.mass + 1/other.mass)
+        self.position -= self.penetration_share*penetration*self.unit_body_normal
+        other.position += other.penetration_share*penetration*self.unit_body_normal
     def update(self, dt):
         self.momentum += self.net_force * dt
         self.velocity = (self.momentum)/self.mass
         self.position = self.position + self.velocity  * dt
         self.net_force = vector(0,0,0) #clears. prevents foce bleed from last frame
-class RotationalMechanics(NewtonianMechanics):
-    def __init__(self, mass, position, moment_of_inertia_com, friction_co, restitution_co, net_torque = None, gravity = None):
-        super().__init__(mass = mass, position = position, friction_co = friction_co, restitution_co = restitution_co, gravity = gravity)
+class RotationalMechanics(Dynamics):
+    def __init__(self, mass, position, moment_of_inertia_com, friction_co, restitution_co, net_torque = None, gravity = None, charge = 0, electric_field = None):
+        super().__init__(mass = mass, position = position, friction_co = friction_co, restitution_co = restitution_co, gravity = gravity, charge = charge, electric_field = electric_field)
         self.moment_of_inertia_com = moment_of_inertia_com
         self.net_torque = net_torque if net_torque is not None else vector(0,0,0)
     def add_torque(self, torque_vector):
@@ -72,6 +80,9 @@ class RotationalMechanics(NewtonianMechanics):
     def gravitation_torque(self):
         self.torque_of_gravity = (self.position - self.axis_of_rotation) ^ self.force_of_gravity
         self.add_torque(self.torque_of_gravity)
+    def electrical_torque(self):
+        self.torque_of_electrostat = (self.position - self.axis_of_rotation) ^ self.electric_force
+        self.add_torque(self.torque_of_electrostat)
     def update(self, dt):
         super().update(dt)
         self.orbital_radius = self.position - self.axis_of_rotation
